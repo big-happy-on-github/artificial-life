c2ae43e4fb2a66e21f60f6099dd90dba7d13bc3e
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-10-01 16:01:16 -0400

    Update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -397,7 +397,7 @@ const occupiedSquares = new Set();
 
 // Draw the grid and the path
 function drawGrid() {
-    ctx.strokeStyle = '#ccc'; // Light gray for grid lines
+    ctx.strokeStyle = '	#3b3b3b'; // Light gray for grid lines
     for (let x = 0; x < canvas.width; x += gridSize) {
         for (let y = 0; y < canvas.height; y += gridSize) {
             ctx.strokeRect(x, y, gridSize, gridSize);

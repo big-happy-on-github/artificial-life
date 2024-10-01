98d5fdf364f7d217288f7e16fed7794a4bd4a36a
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-09-30 21:12:34 -0400

    Update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -310,7 +310,7 @@ class Enemy {
 
 // Define different enemy types with a transformation sequence
 const enemyTypes = [
-    { speed: 1, health: 65, color: 'red', canShoot: false, range: 100, fireRate: 0, damage: 0, level: 1, nextType: 'yellow' }, // Basic enemy
+    { speed: 1, health: 65, color: 'red', canShoot: false, range: 100, fireRate: 0, damage: 0, level: 1, nextType: null }, // Basic enemy
     { speed: 4, health: 45, color: 'orange', canShoot: false, range: 100, fireRate: 0, damage: 0, level: 2, nextType: 'white' }, // Fast enemy
     { speed: 0.7, health: 180, color: 'yellow', canShoot: false, range: 100, fireRate: 0, damage: 0, level: 3, nextType: 'red' }, // Tank enemy, no further transformation
     { speed: 1, health: 70, color: 'green', canShoot: true, range: 100, fireRate: 2000, damage: 25, level: 4, nextType: null }, // Shooting enemy, no further transformation

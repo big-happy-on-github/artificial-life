33218fbabcc21e3fff7dfe3e1ce27259f0993c55
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-10-20 18:27:44 -0400

    Update well-kept.js

--- netlify/functions/well-kept.js
+++ netlify/functions/well-kept.js
@@ -1,6 +1,5 @@
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqZm54eW5udG90dGRieGpjcmVlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODE2MTYzMiwiZXhwIjoyMDQzNzM3NjMyfQ.NLNoMifNOv4seeTLCCV_ZiUmR-YGS7MJnm1bUqZ2B8g';  // Replace with your actual Supabase key
 const supabaseUrl = 'https://kjfnxynntottdbxjcree.supabase.co';  // Replace with your actual Supabase URL
-const screenshotKey = '7cf226';  // Replace with your actual screenshot service key
 
 exports.handler = async (event) => {
     // Access the 'name' parameter from the query string
@@ -9,8 +8,7 @@ exports.handler = async (event) => {
     // Define a mapping for the available keys
     const keys = {
         supabaseKey: supabaseKey,
-        supabaseUrl: supabaseUrl,
-        screenshotKey: screenshotKey
+        supabaseUrl: supabaseUrl
     };
 
     // Check if the requested 'name' exists in the keys object

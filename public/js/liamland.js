520991cce8b4d55bf9ef73293ebbe5b4f7892133
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-10-20 08:19:51 -0400

    Update liamland.js

--- public/js/liamland.js
+++ public/js/liamland.js
@@ -1,8 +1,8 @@
 // Import the Supabase client
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = process.env.supabaseUrl;
-const supabaseKey = process.env.supabaseKey;
+const supabaseUrl = await fetch(`${window.location.origin}/.netlify/functions/well-kept.js?name=supabaseUrl`).json();
+const supabaseKey = await fetch(`${window.location.origin}/.netlify/functions/well-kept.js?name=supabaseKey`).json();
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const gameList = [{ name: "liamstd", cost: 0 }, { name: "chat", cost: 0 }, { name: "wheel", cost: 0 }, { name: "admin", cost: 1/0 }];

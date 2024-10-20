9d204f41ca6576fb85d0131bea1ec61f6025c19b
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-10-20 08:19:33 -0400

    Update liamstd.js

--- public/js/liamstd.js
+++ public/js/liamstd.js
@@ -1538,8 +1538,8 @@ canvas.addEventListener('mousemove', (event) => {
 // Initialize Supabase client
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = process.env.supabaseUrl;
-const supabaseKey = process.env.supabaseKey;
+const supabaseUrl = await fetch(`${window.location.origin}/.netlify/functions/well-kept.js?name=supabaseUrl`).json();
+const supabaseKey = await fetch(`${window.location.origin}/.netlify/functions/well-kept.js?name=supabaseKey`).json();
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Fetch leaderboard and check for duplicate names

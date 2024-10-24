c6d60a2b89c96ad74eea538adb3711321222c0ba
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-10-24 16:06:25 -0400

    Update liamland.js

--- public/js/liamland.js
+++ public/js/liamland.js
@@ -73,10 +73,14 @@ async function addLimbucks(amount, userID, games) {
         // Add or update data in Supabase
         const { data, error } = await supabase
             .from('limbucks')
-            .upsert({ amount: amount, userID: userID, games: games });
+            .replace({ amount: amount, userID: userID, games: games })
+            .eq("userID", userID);
         
         if (error) {
-            throw error;
+            // Add or update data in Supabase
+            const { data, error } = await supabase
+                .from('limbucks')
+                .insert({ amount: amount, userID: userID, games: games })
         }
         
         console.log('Data inserted or updated:', data);

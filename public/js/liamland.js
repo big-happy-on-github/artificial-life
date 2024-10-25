8f85aa935aff3fd570d946227a53c6f561f8555d
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-10-25 16:56:04 -0400

    Update liamland.js

--- public/js/liamland.js
+++ public/js/liamland.js
@@ -110,6 +110,15 @@ async function buy(gameName) {
     }
 }
 
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
 async function updateDisplay() {
     await showGames();
     await getLimbucks();

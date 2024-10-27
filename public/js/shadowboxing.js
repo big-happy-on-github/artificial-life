47f1a507d24d7c161d1c03cb39c8b7b395ffae5d
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-10-27 12:29:15 -0400

    Update shadowboxing.js

--- public/js/shadowboxing.js
+++ public/js/shadowboxing.js
@@ -12,7 +12,7 @@ let enemyMove = null;
 let attacking = 1;
 let combo = []; 
 let requiredCombo = []; // Sequence of moves required before making a new move
-let comboIndex = 0; // Track player's progress in matching the required combo
+let comboIndex = -1; // Track player's progress in matching the required combo
 let playerMoveHistory = { n: 0, e: 0, s: 0, w: 0 };
 
 function dead() {
@@ -29,7 +29,7 @@ function restart() {
     enemyMove = null;
     combo = [];
     requiredCombo = [];
-    comboIndex = 0;
+    comboIndex = -1;
     update();
 }
 
@@ -55,11 +55,11 @@ function update() {
             combo.push(playerMove);
             // Update required combo
             requiredCombo = combo.slice();
-            comboIndex = 0; // Reset combo index after updating required combo
+            comboIndex = -1; // Reset combo index after updating required combo
         } else {
             combo = [];
             requiredCombo = [];
-            comboIndex = 0;
+            comboIndex = -1;
             turn = 1 - turn;
         }
         attacking = turn;
@@ -111,14 +111,13 @@ document.addEventListener('keydown', (event) => {
     // Check if player matches the required combo sequence
     if (requiredCombo.length > 0 && requiredCombo[comboIndex] == playerMove) {
         comboIndex++; // Move to the next required move in the combo sequence
-        console.log(comboIndex, ":", requiredCombo.length);
-        if (comboIndex+1 < requiredCombo.length) {
+        if (comboIndex < requiredCombo.length) {
             playerMove = null; // Not ready for a new move yet
             return;
         }
     } else if (requiredCombo.length > 0) {
         // Reset comboIndex if player doesn't match the required sequence
-        comboIndex = 0;
+        comboIndex = -1;
         playerMove = null;
         alert("You must follow the combo sequence before making a new move.");
         return;

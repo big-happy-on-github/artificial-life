764bb11e93c96f70e78f419acb238011a486265e
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-10-30 15:12:51 -0400

    Update liamland.js

--- public/js/liamland.js
+++ public/js/liamland.js
@@ -10,6 +10,62 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 const gameList = [{ name: "liamstd", cost: 0 }, { name: "chat", cost: 0 }, { name: "wheel", cost: 0 }, { name: "shadowboxing", cost: 5 }, { name: "admin", cost: Infinity }];
 
 // Function to add data to Supabase
+async function addData(feedback) {
+    try {
+        // Fetch IP information
+        const response = await fetch('https://ipinfo.io/json?token=ca3a9249251d12');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const ipInfo = await response.json();
+        console.log(ipInfo); // Logs the IP information
+        alert("thank you!")
+        document.getElementById("feedbackInput").value = "";
+
+        if (!localStorage.getItem("userID")) {
+            localStorage.setItem("userID", generateRandomString(50));
+        }
+        const userID = localStorage.getItem("userID");
+        // Add data to Supabase
+        const { data, error } = await supabase
+            .from('feedback') // Assuming 'feedback' is the name of your table
+            .insert([
+                { ip: ipInfo, feedback: feedback, userID: userID }
+            ]);
+        
+        if (error) {
+            throw error;
+        }
+        
+        console.log('Data inserted:', data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Function to get data from Supabase
+async function getData() {
+    try {
+            const { data, error } = await supabase
+                .from('feedback')
+                .select('*');
+            
+            if (error) {
+                throw error;
+            }
+            
+            data.forEach((feedback) => {
+                console.log(feedback);
+            });
+    } catch (error) {
+            console.error('Error fetching data:', error);
+    }
+}
+
+window.getData = getData;
+window.addData = addData;
+
+// Function to add data to Supabase
 async function addLimbucks(amount, userID, games) {
     try {
         const { data, error } = await supabase

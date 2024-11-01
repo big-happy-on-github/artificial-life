c312f24a386345a30fc0893f68d81a69d687c0c2
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-11-01 16:15:00 -0400

    Update supertag.js

--- public/js/supertag.js
+++ public/js/supertag.js
@@ -48,7 +48,7 @@ function generateRandomObstacles(count) {
             x: Math.random() * (canvas.width - obstacleWidth),
             y: Math.random() * (canvas.height - obstacleHeight),
             width: obstacleWidth,
-            height: obstacleHeight-50
+            height: obstacleHeight-150
         });
     }
     return obstacles;

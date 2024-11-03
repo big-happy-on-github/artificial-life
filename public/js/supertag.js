434f52764151e5225c1c431045273c13c4e650bc
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-11-03 10:53:01 -0500

    Update supertag.js

--- public/js/supertag.js
+++ public/js/supertag.js
@@ -61,6 +61,16 @@ function generateRandomObstacles(count) {
     return obstacles;
 }
 
+// Check collision between two rectangles
+function checkCollision(rect1, rect2) {
+    return (
+        rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.y + rect1.height > rect2.y
+    );
+}
+
 function checkSpec(player) {
     const currentTime = Date.now();
 
